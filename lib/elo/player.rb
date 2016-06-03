f8920c5709dfb1ed263fb21291678033cc34ba49
Brandon Weaver <keystonelemur@gmail.com>
2016-06-02 22:11:23 -0700

    took care of mixed tabs / spaces

--- lib/elo/player.rb
+++ lib/elo/player.rb
@@ -1,8 +1,6 @@
 module Elo
-
   # A player. You need at least two play a Game.
   class Player
-
     include Helper
 
     # The rating you provided, or the default rating from configuration
@@ -10,54 +8,54 @@ module Elo
       @rating ||= Elo.config.default_rating
     end
 
-		# The number of games played is needed for calculating the K-factor.
+    # The number of games played is needed for calculating the K-factor.
     def games_played
       @games_played ||= games.size
     end
 
-		# A list of games played by the player.
+    # A list of games played by the player.
     def games
       @games ||= []
     end
 
-		# Is the player considered a pro, because his/her rating crossed
-		# the threshold configured? This is needed for calculating the K-factor.
+    # Is the player considered a pro, because his/her rating crossed
+    # the threshold configured? This is needed for calculating the K-factor.
     def pro_rating?
       rating >= Elo.config.pro_rating_boundry
     end
 
-		# Is the player just starting? Provide the boundry for
-		# the amount of games played in the configuration.
-		# This is needed for calculating the K-factor.
+    # Is the player just starting? Provide the boundry for
+    # the amount of games played in the configuration.
+    # This is needed for calculating the K-factor.
     def starter?
       games_played < Elo.config.starter_boundry
     end
 
-		# FIDE regulations specify that once you reach a pro status
-		# (see +pro_rating?+), you are considered a pro for life.
-		#
-		# You might need to specify it manually, when depending on
-		# external persistence of players.
-		#
-		#		Elo::Player.new(:pro => true)
+    # FIDE regulations specify that once you reach a pro status
+    # (see +pro_rating?+), you are considered a pro for life.
+    #
+    # You might need to specify it manually, when depending on
+    # external persistence of players.
+    #
+    #   Elo::Player.new(:pro => true)
     def pro?
       !!@pro
     end
 
     # You can override this method if you store each game
-		# in a database or something like that.
-		# This method will be called when a result is known.
+    # in a database or something like that.
+    # This method will be called when a result is known.
     def save
     end
 
-		# Calculates the K-factor for the player.
-		# Elo allows you specify custom Rules (see Elo::Configuration).
-		#
-		# You can set it manually, if you wish:
-		#
-		#		Elo::Player.new(:k_factor => 10)
-		#
-		#	This stops this player from using the K-factor rules.
+    # Calculates the K-factor for the player.
+    # Elo allows you specify custom Rules (see Elo::Configuration).
+    #
+    # You can set it manually, if you wish:
+    #
+    #   Elo::Player.new(:k_factor => 10)
+    #
+    # This stops this player from using the K-factor rules.
     def k_factor
       return @k_factor if @k_factor
       Elo.config.applied_k_factors.each do |rule|
@@ -66,26 +64,26 @@ module Elo
       Elo.config.default_k_factor
     end
 
-		# Start a game with another player. At this point, no
-		# result is known and nothing really happens.
+    # Start a game with another player. At this point, no
+    # result is known and nothing really happens.
     def versus(other_player, options = {})
       Game.new(options.merge(:one => self, :two => other_player)).calculate
     end
 
-		# Start a game with another player and set the score
-		# immediately.
+    # Start a game with another player and set the score
+    # immediately.
     def wins_from(other_player, options = {})
       versus(other_player, options).win
     end
 
-		# Start a game with another player and set the score
-		# immediately.
+    # Start a game with another player and set the score
+    # immediately.
     def plays_draw(other_player, options = {})
       versus(other_player, options).draw
     end
 
-		# Start a game with another player and set the score
-		# immediately.
+    # Start a game with another player and set the score
+    # immediately.
     def loses_from(other_player, options = {})
       versus(other_player, options).lose
     end
@@ -108,7 +106,5 @@ module Elo
       @pro    = true if pro_rating?
       save
     end
-
   end
-
 end
